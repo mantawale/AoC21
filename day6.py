57e778b3ccd5b2b753692e267e737e6d5bdd1e49
mantawale <41280525+mantawale@users.noreply.github.com>
2021-12-06 16:14:12 -0800

    reduced unnecessary complexity

--- day6.py
+++ day6.py
@@ -2,7 +2,7 @@ def process_fishcount(input_census):
 
     popl_buckets = []
     for i in range (0, 9):
-        popl_buckets.append([str(i), input_census.count(str(i))])
+        popl_buckets.append(input_census.count(str(i)))
 
     return popl_buckets 
 
@@ -12,16 +12,16 @@ def repro_cycle(population, time_scale):
         cycle_step = [0 for x in population]
 
         for i in range(0, 8):
-            cycle_step[i] = population[i+1][1]
-        cycle_step[8] = population[0][1]
-        cycle_step[6] += population[0][1]
+            cycle_step[i] = population[i+1]
+        cycle_step[8] = population[0]
+        cycle_step[6] += population[0]
         
         for i in range(0, 9):
-            population[i][1] = cycle_step[i]
+            population[i] = cycle_step[i]
             
         time_scale -= 1
 
-    return sum([x[1] for x in population])
+    return sum(population)
 
 with open('day6input.txt') as input:
     lanternfish_popl = process_fishcount(input.read().split(','))
