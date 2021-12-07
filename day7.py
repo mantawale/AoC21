14512cf48b839267a42af95bc09236cd29c1d783
mantawale <41280525+mantawale@users.noreply.github.com>
2021-12-06 23:36:45 -0800

    minor admin change

--- day7.py
+++ day7.py
@@ -3,16 +3,16 @@ with open('day7input.txt') as input:
 crab_positions.sort()
 
 median = crab_positions[len(crab_positions) // 2]
-burn = sum([abs(i - median) for i in crab_positions])
+min_cburn = sum([abs(i - median) for i in crab_positions])
 
 def vari_burn(a, b):
     return (abs(b - a) / 2) * (abs(b - a) + 1)
 
-def min_burn(input_positions):
+def min_vburn(input_positions):
     burns = []
     for x in range(int(min(input_positions)), int(max(input_positions)) + 1):
         burns.append(sum([vari_burn(x, i) for i in input_positions]))
     return int(min(burns))
 
-print(burn)
-print(min_burn(crab_positions))
+print(min_cburn)
+print(min_vburn(crab_positions))
